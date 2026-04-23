0305228027731f02078a0c8c4e67ae9aa5a95709
HarrisonAbraham <harrison.abraham@ti.ukdw.ac.id>
2026-04-23 08:48:42 +0700

    Update Login
    Implemented user authentication in handleLogin method.

--- src/main/java/org/rplbo/app/ug8/Controller/LoginController.java
+++ src/main/java/org/rplbo/app/ug8/Controller/LoginController.java
@@ -6,27 +6,33 @@ import javafx.event.ActionEvent;
 import org.rplbo.app.ug8.UmbrellaApp;
 import org.rplbo.app.ug8.UmbrellaDBManager;
 
+import java.io.IOException;
+
 public class LoginController {
     @FXML private TextField txtUsername;
     @FXML private PasswordField txtPassword;
     @FXML private Label lblStatus;
 
     @FXML
-    private void handleLogin(ActionEvent event) {
-        // ==============================================================================
-        // TODO 1: PROSES AUTENTIKASI (LOGIN)
-        // ==============================================================================
-        // 1. Ambil input teks dari txtUsername dan txtPassword.
-        // 2. Buat instansiasi dari class UmbrellaDBManager.
-        // 3. Panggil metode validateUser() dari db manager tersebut.
-        // 4. Jika hasil validasi berhasil (tidak null):
-        //    a. Simpan nama user ke variabel statis UmbrellaApp.loggedInUser.
-        //    b. Pindah ke halaman "umbrella-view.fxml" menggunakan UmbrellaApp.switchScene().
-        // 5. Jika gagal, tampilkan pesan error "AUTHENTICATION FAILED" pada lblStatus.
-        // ==============================================================================
+    private void handleLogin(ActionEvent event) throws IOException {
+        String username = txtUsername.getText();
+        String password = txtPassword.getText();
+        UmbrellaDBManager dbManager = new UmbrellaDBManager();
 
-        // --- TULIS KODE ANDA DI BAWAH INI ---
+        String fullName = dbManager.validateUser(username, password);
 
 
+        if (fullName != null) {
+
+            UmbrellaApp.loggedInUser = fullName;
+
+            try {
+                UmbrellaApp.switchScene("umbrella-view.fxml");
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        } else {
+            lblStatus.setText("AUTHENTICATION FAILED");
+        }
     }
 }
